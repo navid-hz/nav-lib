31775f566f9fa077f4a8abc2f5413f98bdb833f2
Navid Hosseini <hosseini.zari@gmail.com>
2024-05-20 19:08:41 +0200

    change

---
+++ src/btn/btn.stories.js
@@ -0,0 +1,16 @@
+
+import Button from "./button";
+
+export default {
+    title: 'Button',
+    component: Button,
+    argTypes: {
+        lable: { control: 'text' },
+    }
+};
+
+export const Text = {
+    args: {
+        label: 'Hello Button'
+    }
+}
--- src/btn/button.jsx
+++ src/btn/button.jsx
@@ -1,34 +1,10 @@
 import React from 'react';
-import StyledButton from './button.styles';
-import PropTypes from 'prop-types';
 
-// Component
-const Button = ({ label, backgroundColor, size, onClick, ...props }) => {
+
+const Button = ({ label }) => {
   return (
-    <StyledButton size={size} backgroundColor={backgroundColor} {...props}>
-      {label}
-    </StyledButton>
+    <button>{label}</button>
   );
-};
-
-// Props
-Button.propTypes = {
-  label: PropTypes.string.isRequired,
-  size: PropTypes.oneOf(['small', 'big', 'large']),
-  backgroundColor: PropTypes.string.isRequired,
-  children: PropTypes.node,
-  onClick: PropTypes.func,
-};
-
-Button.propTypes = {
-  label: PropTypes.string,
-};
-
-// Defult Props
-Button.defaultProps = {
-  label: 'Contact Us',
-  size: 'small',
-  backgroundColor: 'defult',
 };
 
 export default Button;
