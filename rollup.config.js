b38364a9ea62ed70f07df0e700a71cfbcf592d5d
Navid Hosseini <hosseini.zari@gmail.com>
2024-05-20 12:32:50 +0200

    final touch

--- rollup.config.js
+++ rollup.config.js
@@ -2,9 +2,11 @@ import babel from '@rollup/plugin-babel';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import terser from '@rollup/plugin-terser';
-import pkg from './package.json' assert { type: "json" };
+//import pkg from './package.json' assert { type: 'json' };
 import image from '@rollup/plugin-image';
 import postcss from 'rollup-plugin-postcss';
+
+const pkg = require('./package.json');
 
 export default {
     input:'src/index.js',
